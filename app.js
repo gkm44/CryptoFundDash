239f4605ecaef8715bda93af0ddf827b7e70dc8c
gkm44 <101818632+gkm44@users.noreply.github.com>
2022-04-12 04:33:18 +0200

    timer

--- app.js
+++ app.js
@@ -19,9 +19,12 @@ const app = Vue.createApp({
   
       getQuotes() {
         this.AXS_quote();
+        setInterval(this.AXS_quote,1000);
         this.BTC_quote();
+        setInterval(this.BTC_quote,1000);
       },
   
+
       async AXS_quote() {
         try {
           let Aresponse = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=axie-infinity&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d");
